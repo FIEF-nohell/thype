da55b7a0c1c6a8ef7a19d6b7f5ac817d4c0f01d7
FIEF-nohell <noel.hmnn@gmail.com>
2022-10-24 15:07:28 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Thype',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
