1755e6c8b458be206de5806aa42e98ccc32b23cf
FIEF-nohell <noel.hmnn@gmail.com>
2022-10-24 16:56:35 +0200

    changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,6 +10,11 @@ export class HomePage {
   type_clicked: boolean = true;
   stats_clicked: boolean = false;
   settings_clicked: boolean = false;
+  average_english_word_length: any = 4.7;
+  small_letters: any[] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  big_letters: any[] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  symbols: any[] = ["!", '"', "§", "%", "&", "/", "(", ")", "=", "?", "@", "{", "[", "]", "}", ",", ".", ";", ":", "-", "_", "#", "'", "*", "*", "<", ">"];
+  numbers: any[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
   main_page(){
 
@@ -36,5 +41,8 @@ export class HomePage {
     this.settings_clicked = true;
   }
 
+  generate_new_letter(){
+
+  }
 
 }
